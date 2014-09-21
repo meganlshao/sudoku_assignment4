1a6b590d98e8d257149d86021dc9f7b6a0bec2b7
Kate Aplin <kaplin@hmc.edu>
2014-09-20 20:17:18 -0700

    Create gridModel class.

---
+++ sudoku/KAMSGridModel.h
@@ -0,0 +1,13 @@
+//
+//  KAMSGridModel.h
+//  sudoku
+//
+//  Created by Kathryn Aplin on 9/20/14.
+//  Copyright (c) 2014 Kathryn Aplin Megan Shao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KAMSGridModel : NSObject
+
+@end
---
+++ sudoku/KAMSGridModel.m
@@ -0,0 +1,13 @@
+//
+//  KAMSGridModel.m
+//  sudoku
+//
+//  Created by Kathryn Aplin on 9/20/14.
+//  Copyright (c) 2014 Kathryn Aplin Megan Shao. All rights reserved.
+//
+
+#import "KAMSGridModel.h"
+
+@implementation KAMSGridModel
+
+@end
