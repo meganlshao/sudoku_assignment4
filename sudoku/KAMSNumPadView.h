252364e6523e1afdaf56dd7a4e0f9e5280920a73
Kate Aplin <kaplin@hmc.edu>
2014-09-20 20:58:50 -0700

    Created NumPadView class.

---
+++ sudoku/KAMSNumPadView.h
@@ -0,0 +1,13 @@
+//
+//  KAMSNumPadView.h
+//  sudoku
+//
+//  Created by Kathryn Aplin on 9/20/14.
+//  Copyright (c) 2014 Kathryn Aplin Megan Shao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KAMSNumPadView : UIView
+
+@end
---
+++ sudoku/KAMSNumPadView.m
@@ -0,0 +1,31 @@
+//
+//  KAMSNumPadView.m
+//  sudoku
+//
+//  Created by Kathryn Aplin on 9/20/14.
+//  Copyright (c) 2014 Kathryn Aplin Megan Shao. All rights reserved.
+//
+
+#import "KAMSNumPadView.h"
+
+@implementation KAMSNumPadView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
